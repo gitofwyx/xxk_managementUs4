51f3b930e85b000a01549d5d2afe7b558e04522f
WYXofGit <>
2018-04-17 17:42:36 +0800

    2018年4月17日 17:42:33

--- src/main/java/com/xxk/management/system/controller/AuthController.java
+++ src/main/java/com/xxk/management/system/controller/AuthController.java
@@ -3,10 +3,7 @@ package com.xxk.management.system.controller;
 import com.xxk.core.file.BaseController;
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
@@ -35,22 +32,48 @@ public class AuthController extends BaseController {
     }
 
     @RequestMapping(value = "/login")//用户登录路径
-    public String login(HttpServletRequest request) {
+    public ModelAndView login(HttpServletRequest request) {
         // shiro在认证过程中出现错误后将异常类路径通过request返回
-        String exceptionClassName = (String) request.getAttribute("shiroLoginFailure");
-        if(exceptionClassName!=null){
-            if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
-                System.out.println("账号不存在");
-            } else if (IncorrectCredentialsException.class.getName().equals(
-                    exceptionClassName)) {
-                System.out.println("用户名/密码错误");
-            } else if("randomCodeError".equals(exceptionClassName)){
-                System.out.println("验证码错误");
-            } else{
-                System.out.println("未知错误");
+        Subject currentUser = SecurityUtils.getSubject();
+        Session session = currentUser.getSession();
+        UsernamePasswordToken token = null;
+        Map<String, String> result = new HashMap<>();
+        String username = (String) request.getParameter("username");
+        String password = (String) request.getParameter("password");
+        if (!currentUser.isAuthenticated()) {
+            String exceptionClassName = (String) request.getAttribute("shiroLoginFailure");
+            if (exceptionClassName != null) {
+                if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
+                    result.put("error", "帐号未注册");
+                } else if (IncorrectCredentialsException.class.getName().equals(
+                        exceptionClassName)) {
+                    result.put("error", "用户名/密码错误");
+                } else if ("randomCodeError".equals(exceptionClassName)) {
+                    result.put("error", "验证码错误");
+                } else {
+                    result.put("error", "系统错误");
+                }
             }
-            return "redirect:/";
+        } else if (!"".equals(username) && username != null) {
+            token = new UsernamePasswordToken(username, password);// 获取登录令牌
+            try {
+                //token.setRememberMe(true);
+                currentUser.login(token);
+                result.put("success", "已登录");
+                return new ModelAndView("/main", "result", result);
+            } catch (UnknownAccountException uae) {
+                result.put("error", "帐号未注册");
+            } catch (IncorrectCredentialsException ice) {
+                result.put("error", "用户名/密码错误");
+            } catch (ExcessiveAttemptsException eae) {
+                // 捕获错误登录过多的异常
+                result.put("error", "登录超次");
+            } catch (AuthenticationException ae) {
+                result.put("error", "系统错误");
+            }
+        } else {
+            result.put("warnings", ",重新登录请先注销");
         }
-        return "redirect:/main";
+        return new ModelAndView("/index", "result", result);
     }
 }
