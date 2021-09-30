87090845c9351a0d2eecf2f2dfda701dfeb61178
gitofwyx <919921423@qq.com>
2021-10-01 03:31:30 +0800

    2021年10月1日 03:30:18 Bootstrap响应式布局

--- src/main/webapp/WEB-INF/res/assets/js/suggest/suggest_util.js
+++ src/main/webapp/WEB-INF/res/assets/js/suggest/suggest_util.js
@@ -34,3 +34,25 @@ function buildBsSuggest(cla, v, search, effective, onSet_ck,onUnset_ck) {
 
 
 };
+//下拉提示框bsSuggest
+function buildBsSuggestIO(cla,options, onSet_ck,onUnset_ck) {
+
+    $("#" + cla).bsSuggest('init', options).on('onDataRequestSuccess', function (e, result) {
+        console.log('onDataRequestSuccess: ', result);
+    }).on('onSetSelectValue', function (e, keyword, data) {
+        console.log('onSetSelectValue: ', keyword, data);
+        //回调函数
+        if (typeof onSet_ck === "function") {
+            //调用它，既然我们已经确定了它是可调用的
+            onSet_ck(keyword, data);
+        }
+
+    }).on('onUnsetSelectValue', function () {
+        if (typeof onUnset_ck === "function") {
+            //调用它，既然我们已经确定了它是可调用的
+            onUnset_ck();
+        }
+    });
+
+
+};
