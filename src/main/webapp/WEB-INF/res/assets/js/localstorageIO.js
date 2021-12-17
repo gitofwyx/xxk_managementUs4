99946f9eb94428dcef7ed1057dfebe846b490df5
gitofwyx <919921423@qq.com>
2021-12-18 03:01:32 +0800

    2021年12月13日 00:48:11 log4j升级2.15.0

--- src/main/webapp/WEB-INF/res/assets/js/localstorageIO.js
+++ src/main/webapp/WEB-INF/res/assets/js/localstorageIO.js
@@ -1,38 +1,38 @@
 function localstorageIO(ioKey, url, isReset, ioValue) {
     if (window.localStorage && (window.localStorage.setItem('a', 123) , window.localStorage.getItem('a') == 123)) {
         var stroage = window.localStorage;
-            if (ioValue != undefined && ioValue != null) {
-                stroage.setItem(ioKey, ioValue);
-                return null;
-            }
-            if (url != undefined && url != null) {
-                $.ajax({
-                    url: url,
-                    data: {"updateDate":stroage.getItem("OfficeUpdate")},
-                    type: 'POST', //GET
-                    async: true,    //或false,是否异步
-                    /*timeout: 5000,*/    //超时时间(如果后台数据加载缓慢，设置超时时间会导致在时间超时后自动执行success函数而后台还没有返回data数据)
-                    dataType: 'json',    //返回的数据格式：json/xml/html/script/jsonp/text
-                    success: function (data, textStatus, jqXHR) {
-                        if (data != null && data != undefined) {
-                            if(data.hasError==true){
-                                console.log('更新失败：'+data.error);
-                                return;
-                            }
-                            stroage.setItem(ioKey, JSON.stringify(data.result));
-                            stroage.setItem("OfficeUpdate", data.updateDate);
-                            console.log('更新科室完成，'+stroage.getItem("OfficeUpdate"));
-                        }
-                    },
-                    error: function (xhr, textStatus) {
-                        console.log('错误')
-                        console.log(xhr)
-                        console.log(textStatus)
-                    }
-                })
-            } else {
-            return JSON.parse(stroage.getItem(ioKey));
+        if (ioValue != undefined && ioValue != null) {
+            stroage.setItem(ioKey, ioValue);
+            return null;
         }
+        if (url != undefined && url != null) {
+            $.ajax({
+                url: url,
+                data: {"updateDate": stroage.getItem("OfficeUpdate")},
+                type: 'POST', //GET
+                async: true,    //或false,是否异步
+                /*timeout: 5000,*/    //超时时间(如果后台数据加载缓慢，设置超时时间会导致在时间超时后自动执行success函数而后台还没有返回data数据)
+                dataType: 'json',    //返回的数据格式：json/xml/html/script/jsonp/text
+                success: function (data, textStatus, jqXHR) {
+                    if (data != null && data != undefined) {
+                        if (data.hasError == true) {
+                            console.log('更新失败：' + data.error);
+                            return;
+                        }
+                        stroage.setItem(ioKey, JSON.stringify(data.result));
+                        stroage.setItem("OfficeUpdate", data.updateDate);
+                        console.log('更新科室完成，' + stroage.getItem("OfficeUpdate"));
+                    }
+                },
+                error: function (xhr, textStatus) {
+                    console.log('错误')
+                    console.log(xhr)
+                    console.log(textStatus)
+                }
+            })
+        }
+        return JSON.parse(stroage.getItem(ioKey));
+
     }
     return null;
 }
