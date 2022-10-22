3f536832b96bd4a1140c3dcd0e8ca71e8d223950
WYX1993-PC <919921423@qq.com>
2022-10-22 16:49:26 +0800

    2022年1月25日 03:34:14

--- src/main/webapp/WEB-INF/res/assets/js/suggest/suggest_util.js
+++ src/main/webapp/WEB-INF/res/assets/js/suggest/suggest_util.js
@@ -94,6 +94,29 @@ function buildBsSuggestIO(cla,options, onSet_ck,onUnset_ck) {
 
 };
 
+function buildBsSuggestOnRequest(cla,options,onReq_ck, onSet_ck,onUnset_ck) {
+
+    $("#" + cla).bsSuggest('init', options).on('onDataRequestSuccess', function (e, result) {
+        console.log('onDataRequestSuccess: ', result);
+        if (typeof onReq_ck === "function") {
+            //调用它，既然我们已经确定了它是可调用的
+            onReq_ck(e, result);
+        }
+    }).on('onSetSelectValue', function (e, keyword, data) {
+        console.log('onSetSelectValue: ', keyword, data);
+        //回调函数
+        if (typeof onSet_ck === "function") {
+            //调用它，既然我们已经确定了它是可调用的
+            onSet_ck(keyword, data);
+        }
+    }).on('onUnsetSelectValue', function () {
+        if (typeof onUnset_ck === "function") {
+            //调用它，既然我们已经确定了它是可调用的
+            onUnset_ck();
+        }
+    });
+};
+
 function initBsSuggest(cla) {
 
     $("#" + cla).bsSuggest('destroy');
